575422791cc2b588c0faad46c305c4985f2e0543
OmaymaK <emmablink050@gmail.com>
2022-06-14 17:38:03 +0100

    task2

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+include"sort.h"
+void swap(int *xp, int *yp);
+/**
+ * swap - function that swapto number.
+ * @xp: first nmber.
+ * @yp: second number.
+ */
+
+void swap(int *xp, int *yp)
+{
+	int tmp = *xp;
+	*xp = *yp;
+	*yp = tmp;
+}
+/**
+ * selection_sort - function that sorts an array  using the Selection sort.
+ * @array: table of array.
+ * @size: size of array.
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		k = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[k])
+			{
+				k = j;
+			}
+		if (k != i)
+		{
+			swap(&array[k], &array[i]);
+			print_array(array, size);
+		}
+	}
+}
